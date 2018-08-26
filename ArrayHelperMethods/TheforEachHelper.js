9b0e2056eeff054d4a596c4bfd5d81f715355a95
Serina Ko <serinako08@gmail.com>
2018-08-26 17:09:52 +1000

    use the forEach helper to process values

--- ArrayHelperMethods/TheforEachHelper.js
+++ ArrayHelperMethods/TheforEachHelper.js
@@ -29,3 +29,48 @@ const adder = (number) => {
 numbers.forEach(adder);
 // print the sum variable
 sum;
+
+// 3) refactor the code below
+// function handlePosts() {
+//     var posts = [
+//       { id: 23, title: 'Daily JS News' },
+//       { id: 52, title: 'Code Refactor City' },
+//       { id: 105, title: 'The Brightest Ruby' }
+//     ];
+
+//     for (var i = 0; i < posts.length; i++) {
+//       savePost(posts[i]);
+//     }
+// }
+// solution
+// const handlePosts = () => {
+//     const posts = [
+//         { id: 23, title: 'Daily JS News' },
+//         { id: 52, title: 'Code Refactor City' },
+//         { id: 105, title: 'The Brightest Ruby' }
+//     ]
+
+//     posts.forEach((post) = {
+//         savePost(post)
+//     })
+// }
+
+// 4) processing values
+const images = [
+  { height: 10, width: 30 },
+  { height: 20, width: 90 },
+  { height: 54, width: 32 }
+];
+
+const areas = [];
+images.forEach((image) => {
+  areas.push(image.height * image.width);
+});
+console.log(areas);
+
+// const areas = [];
+// images.forEach(function(image) {
+//   let area = image.height * image.width;
+//   areas.push(area);
+// });
+// console.log(areas);
