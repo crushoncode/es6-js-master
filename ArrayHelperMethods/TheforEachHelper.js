7951742be6ba9b15b7e3daf51717949eb1a90510
Serina Ko <serinako08@gmail.com>
2018-08-26 15:46:18 +1000

    loop over the array, incrementing the sum variable

---
+++ ArrayHelperMethods/TheforEachHelper.js
@@ -0,0 +1,31 @@
+const dev = ['frontend', 'backend', 'devOps'];
+
+// pass each element in the dev array to the iterator function
+dev.forEach((dev) => {
+  console.log(dev);
+});
+
+// 1)
+// Create an array of numbers
+const nums = [1, 2, 3, 4, 5];
+// Create a variable to hold the sum
+let sum = 0;
+// Loop over the array, incrementing the sum variable
+num.forEach((num) => {
+  sum += num;
+});
+// print the sum variable
+sum;
+
+// 2)
+// Create an array of numbers
+const numbers = [1, 2, 3, 4, 5];
+// Create a variable to hold the sum
+let sumValue = 0;
+const adder = (number) => {
+  sumValue += number;
+};
+// Loop over the array, incrementing the sum variable
+numbers.forEach(adder);
+// print the sum variable
+sum;
